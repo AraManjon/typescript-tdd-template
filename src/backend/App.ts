e76c658beaacb494c7934480aba2bc2bb6e2a13f
Ara <ara.manjoncb@gmail.com>
2023-03-29 00:10:02 +0200

    added ts-node-dev dependency and deleted nodemon

--- src/backend/App.ts
+++ src/backend/App.ts
@@ -6,8 +6,6 @@ export class App {
 	async start(): Promise<void> {
 		const port = process.env.PORT ?? "8000";
 		this.server = new Server(port);
-		// eslint-disable-next-line no-debugger
-		debugger;
 
 		await this.server.listen();
 	}
